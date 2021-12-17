6bad712ec054a4a5fddfc1f894d7e9932b772187
leepowe <leepowell2@yahoo.com>
2021-12-16 20:21:12 -0500

    added logo to nav

--- client/js/components/Header.js
+++ client/js/components/Header.js
@@ -1,5 +1,6 @@
 export default function Header(){
     return `
+    <h1 id="header__logo">Lee Powell</h1>
     <ul class='nav-list'>
         <li class='nav-list__home'>Home</li>
         <li class='nav-list__projects'>Projects</li>
